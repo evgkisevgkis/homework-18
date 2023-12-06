4d5c90b6183ad89c09de6617c6f94e2f7359052f
Johnny <evgkisevgkis@gmail.com>
2023-12-06 19:58:35 +0500

    создание модели для фильмов

---
+++ dao/model/movies.py
@@ -0,0 +1,17 @@
+from setup_db import db
+from sqlalchemy.orm import relationship
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    trailer = db.Column(db.String)
+    year = db.Column(db.String)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'))
+    genre = relationship('Genre')
+    director_id = db.Column(db.Integer, db.ForeignKey('director.id'))
+    director = relationship('Director')
+
